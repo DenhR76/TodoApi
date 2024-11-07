8c98c558c723c31dd2e1b28d80d321171fa67679
DenhR76 <zetoryx@gmail.com>
2024-11-07 15:47:28 +0100

    Prevent over-posting

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -7,3 +7,10 @@ public class TodoItem
     public bool IsComplete { get; set; }
     public string? Secret { get; set; }
 }
+
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
